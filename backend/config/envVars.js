84ff58c6ae351dc16e23f3e4087a99cd23dae8c5
Bruno-Rocha-Programmer <robertbruno592@gmail.com>
2025-06-30 21:50:00 -0300

    configuring the app to run on railway

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-  MONGO_URI:
-    "mongodb+srv://robertbruno592:TMrtCvbrf6Ok78PQ@cluster0.t45ouhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,13 @@ import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 
-import dotenv from "dotenv";
+import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
-dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json()); // will allow us to parse req.body
@@ -25,13 +24,15 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-  console.log("Server started http://localhost:" + PORT);
+  console.log("Server started at http://localhost:" + PORT);
   connectDB();
 });
