e198113bc61b0ea176644fda67f7995b800658bf
Bruno-Rocha-Programmer <robertbruno592@gmail.com>
2025-06-30 21:08:07 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Server started at http://localhost:" + PORT);
+  console.log("Server started http://localhost:" + PORT);
   connectDB();
 });
